2d988767645959a69297cc7a0c77d3579fc007cb
tookbra <tookbra@outlook.com>
2020-03-17 17:45:01 +0800

    :bug: fix lombok @Builder冲突

--- reksai-service/reksai-dht-node/src/main/java/org/okboom/reksai/dht/node/props/DhtNode.java
+++ reksai-service/reksai-dht-node/src/main/java/org/okboom/reksai/dht/node/props/DhtNode.java
@@ -2,6 +2,7 @@ package org.okboom.reksai.dht.node.props;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Tolerate;
 
 import java.io.Serializable;
 
@@ -16,4 +17,7 @@ public class DhtNode implements Serializable {
     private String address;
 
     private int port;
+
+    @Tolerate
+    public DhtNode (){}
 }
