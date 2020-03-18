3601643b146f15bd377d2eb8eaebfae76f4f86e3
tookbra <tookbra@outlook.com>
2020-03-18 17:53:28 +0800

    :art: add search module

---
+++ reksai-portal/reksai-protal-web/src/main/java/org/okboom/reksai/dht/web/WebApplication.java
@@ -0,0 +1,13 @@
+package org.okboom.reksai.dht.web;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author tookbra
+ */
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
