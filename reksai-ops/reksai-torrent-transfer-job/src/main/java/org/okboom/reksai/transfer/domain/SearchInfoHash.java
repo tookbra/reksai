ba505a1d885712f51d74d053b69f478f5ac00120
tookbra <tookbra@outlook.com>
2020-03-21 13:04:33 +0800

    :sparkles: mysql transfer to es

--- reksai-ops/reksai-torrent-transfer-job/src/main/java/org/okboom/reksai/transfer/domain/SearchInfoHash.java
+++ reksai-ops/reksai-torrent-transfer-job/src/main/java/org/okboom/reksai/transfer/domain/SearchInfoHash.java
@@ -2,6 +2,7 @@ package org.okboom.reksai.transfer.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -10,6 +11,7 @@ import java.util.Date;
  * @author tookbra
  */
 @Data
+@ToString
 @TableName("search_hash")
 public class SearchInfoHash implements Serializable {
 
--- reksai-ops/reksai-torrent-transfer-job/src/test/java/org/okboom/reksai/transfer/TransferTest.java
+++ reksai-ops/reksai-torrent-transfer-job/src/test/java/org/okboom/reksai/transfer/TransferTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author tookbra
@@ -39,33 +40,43 @@ public class TransferTest {
 
     @Test
     public void test1() {
-        System.out.println(18000%1000);
+        Page<SearchInfoHash> page = new Page<SearchInfoHash>(320, 10000);
+        IPage<SearchInfoHash> iPage = searchInfoHashMapper.selectPage(page, new QueryWrapper<>());
+        System.out.println(1);
     }
 
-    @Test
-    public void testTransfer() {
-        Page<SearchInfoHash> page;
-        int count = searchInfoHashMapper.selectCount(new QueryWrapper<>());
-        int size = (count / 1000) + ((count % 1000) > 0 ? 1 : 0);
-        for(int i = 0; i < size; i++) {
-            page = new Page<SearchInfoHash>(i + 1, 1000);
-            IPage<SearchInfoHash> iPage = searchInfoHashMapper.selectPage(page, new QueryWrapper<>());
-            List<SearchInfoHash> searchInfoHashes = page.getRecords();
-            if(CollectionUtils.isEmpty(searchInfoHashes)) {
-                break;
-            }
 
-            List<TorrentDocument> queries = new ArrayList();
-            iPage.getRecords().forEach(searchInfoHash ->  {
-                TorrentDocument torrentDocument = new TorrentDocument();
-                torrentDocument.setInfoHash(searchInfoHash.getInfoHash());
-                torrentDocument.setFileName(searchInfoHash.getName());
-                torrentDocument.setFileSize(searchInfoHash.getLength());
-                torrentDocument.setFileType(searchInfoHash.getCategory());
-                torrentDocument.setCreateDate(searchInfoHash.getCreateTime().getTime());
-                queries.add(torrentDocument);
+    @Test
+    public void testTransfer() throws InterruptedException {
+        Executor executor = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+        int count = searchInfoHashMapper.selectCount(new QueryWrapper<>());
+        int size = (count / 10000) + ((count % 10000) > 0 ? 1 : 0);
+        final CountDownLatch countDownLatch = new CountDownLatch(size);
+        for(int i = 1; i <= size; i++) {
+            int finalI = i;
+            executor.execute(() -> {
+                Page<SearchInfoHash> page = new Page<SearchInfoHash>(finalI, 10000);
+                IPage<SearchInfoHash> iPage = searchInfoHashMapper.selectPage(page, new QueryWrapper<>());
+                List<SearchInfoHash> searchInfoHashes = page.getRecords();
+                if(CollectionUtils.isNotEmpty(searchInfoHashes)) {
+                    List<TorrentDocument> queries = new ArrayList();
+                    iPage.getRecords().forEach(searchInfoHash ->  {
+                        TorrentDocument torrentDocument = new TorrentDocument();
+                        torrentDocument.setInfoHash(searchInfoHash.getInfoHash());
+                        torrentDocument.setFileName(searchInfoHash.getName());
+                        torrentDocument.setFileSize(searchInfoHash.getLength());
+                        torrentDocument.setFileType(searchInfoHash.getCategory());
+                        torrentDocument.setCreateDate(searchInfoHash.getCreateTime().getTime());
+                        queries.add(torrentDocument);
+                    });
+                    torrentIndexRepository.saveAll(queries);
+                }
+                System.out.println("===============" + finalI + "==============");
+                countDownLatch.countDown();
             });
-            torrentIndexRepository.saveAll(queries);
+
         }
+        countDownLatch.await();
+        System.out.println("===============处理完成==============");
     }
 }
